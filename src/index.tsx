42c584a255276e97931ec550dbbea2ed391951e1
[dyad] <scooter.vineburgh@gmail.com>
2025-07-20 17:03:42 -0500

    [dyad] Rendering the main application component - wrote 1 file(s), renamed 1 file(s)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import MainApp from './MainApp'; // Import the main application component
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
 );
 
