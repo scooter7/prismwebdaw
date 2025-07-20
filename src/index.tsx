d6fea213f72409e5f82049ccfedc8f204e75b29e
[dyad] <scooter.vineburgh@gmail.com>
2025-07-20 17:06:19 -0500

    [dyad] Moving BlueprintJS CSS imports to index.tsx - wrote 2 file(s)

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom/client';
 import MainApp from './MainApp'; // Import the main application component
 import reportWebVitals from './reportWebVitals';
 
-import './index.css';
+import './index.css'; // Your custom CSS
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/select/lib/css/blueprint-select.css';
+import '@blueprintjs/table/lib/css/blueprint-table.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
