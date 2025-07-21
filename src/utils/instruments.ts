fc49f95caf1f0e8577bc36da7480adfff5bb76a8
[dyad] <scooter.vineburgh@gmail.com>
2025-07-21 16:22:00 -0500

    [dyad] Fixed AI drum track creation by supporting the Drums instrument in createInstrument. - wrote 1 file(s)

--- src/utils/instruments.ts
+++ src/utils/instruments.ts
@@ -1,13 +1,17 @@
 import { Analog } from '../instruments/Analog';
+import { Drums } from '../instruments/Drums';
 import { Instrument } from '../core/Instrument';
 
 export function createInstrument(name: string): Instrument {
   const lowerName = name.toLowerCase();
-  
+
   if (lowerName === 'analog') {
     return new Analog();
   }
-  
+  if (lowerName === 'drums') {
+    return new Drums();
+  }
+
   // Default to Analog for any other request.
   console.warn(`Instrument '${name}' not found, defaulting to Analog.`);
   return new Analog();
